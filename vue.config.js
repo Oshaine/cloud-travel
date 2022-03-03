53798183f2686b93dcce586925c4be99ce152657
Oshaine <oshaine429smith@gmail.com>
2022-03-03 15:06:30 -0500

    update

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
-  devServer: {
-    proxy: "https://hiring.zumata.xyz",
-  },
+  // devServer: {
+  //   proxy: "https://hiring.zumata.xyz",
+  // },
 });
