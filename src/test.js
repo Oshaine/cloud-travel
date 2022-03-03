096c609646c58ea76bd45c747685b3e0928db2e8
Oshaine <oshaine429smith@gmail.com>
2022-03-03 15:07:59 -0500

    updates

--- src/test.js
+++ src/test.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 let BaseApi = axios.create({
-  baseURL: process.env.VUE_APP_URL,
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? process.env.BASE_URL_PROD
+      : process.env.BASE_URL_DEV,
 });
 
 export default BaseApi;
