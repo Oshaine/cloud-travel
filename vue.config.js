238dbad0acf5b5e549da732cc644488378f4abbe
Oshaine <oshaine429smith@gmail.com>
2022-03-03 15:35:57 -0500

    updates

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production"
-      ? process.env.BASE_URL_PROD
-      : process.env.BASE_URL_DEV,
+  //   publicPath:
+  //     process.env.NODE_ENV === "production"
+  //       ? process.env.BASE_URL_PROD
+  //       : process.env.BASE_URL_DEV,
 
   devServer: {
     proxy: process.env.BASE_URL_PROD,
